f0c258ec752e442c774a28615f8bb85aac1c3f60
sebastian <sebastian@sommerfeld.io>
2022-09-25 21:59:24 +0200

    feat: prepared entrypoint

--- src/main/entrypoint.sh
+++ src/main/entrypoint.sh
@@ -1,6 +1,57 @@
 #!/bin/bash
+# @file entrypoint.sh
+# @brief Entrypoint for Github Action.
+#
+# @description Entrypoint for Github Action. This script updates the version in a projects antora.yml and sets either `main` or `<branchname>` depending on the current branch.
+#
+# IMPORTANT: Do not run this script directly. This script is intended to run as part of a Github Actions job.
+#
+# ==== Arguments
+#
+# The script does not accept any parameters.
 
-echo "TODO ..."
+
+ANTORA_YML="docs/antora.yml"
+BRANCH="${{github.ref}}"
+
+
+# @description Adjust version from 'main' to '<branchname>' or to '<version>' for release branches (if not already correct).
+function mainToBranchname() {
+    old="refs/heads/"
+    BRANCH="${BRANCH//$old/}"
+
+    # antora does not allow slashes in versions
+    old="/"
+    new="__"
+    BRANCH="${BRANCH/$old/$new}"
+
+    # remove the prefix for release branches
+    old="release__"
+    BRANCH="${BRANCH/$old/}"
+
+    oldPattern='version: '
+    new="version: $BRANCH"
+    sed -i "/$oldPattern/s/.*/$new/" "$ANTORA_YML"
+}
+
+
+# @description Adjust version from '<branchname>' to 'main' if not already correct ... run on main branch.
+function branchnameToMain() {
+    oldPattern='version: '
+    new='version: main'
+    sed -i "/$oldPattern/s/.*/$new/" "$ANTORA_YML"
+}
+
+
+echo "Update version in $ANTORA_YML"
+echo "Current ref = $BRANCH"
+case "$BRANCH" in 
+  *"refs/heads/feat/"* ) mainToBranchname ;;
+  *"refs/heads/release/"* ) mainToBranchname ;;
+  *"refs/heads/main"* ) branchnameToMain ;;
+esac
 
 pwd
 ls -alF
+
+cat "$ANTORA_YML"
